cb5a9622544c6b142a8e269470d1aab2fe9331a7
Daniel M <mcmanimoncoding@gmail.com>
2019-09-29 19:59:32 -0700

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Lets Make Something New</h1>
+    <p>Designing something new can be scary, but we have to do what we can!</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/about-me/">About Me</Link>
   </Layout>
 )
 
